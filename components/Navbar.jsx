fa4a3088b4e55534968c1cf287771287c7add8b1
PedroPini <augustopini@gmail.com>
2023-08-06 22:06:20 +0800

    next/link implementation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react"
 import Image from 'next/image'
+import Link from 'next/link'
 const Navbar =() =>{
 // State to track the visibility of the mobile menu
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,10 +13,10 @@ const Navbar =() =>{
 
 <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="#" className="flex items-center">
+  <Link href="#" className="flex items-center">
       <Image src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Reebelo Logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Reebelo</span>
-  </a>
+  </Link>
   <button onClick={toggleMenu} data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded={isMenuOpen}>
       <span className="sr-only">Open mobile menu</span>
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -27,11 +28,11 @@ const Navbar =() =>{
           } w-full md:block md:w-auto`} id="navbar-multi-level">
     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <li>
-        <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Products</a>
+        <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Products</Link>
       </li>
       
       <li>
-        <a href="/orders" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Orders</a>
+        <Link href="/orders" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Orders</Link>
       </li>
       
     </ul>
